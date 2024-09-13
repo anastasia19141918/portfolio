c8098fcb3bd6d3576002c9ba80eec748d69825e7
Anastasiya <anastasiya.anast.ru@mail.ru>
2024-09-13 13:17:51 +0300

    feat: add link and media link

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,7 +1,20 @@
-const burger =  document.getElementById('burger');
+const burger = document.getElementById('burger');
 const burgerItemns = document.getElementById('header__itemns');
 
 burger.addEventListener('click', function(){
   burger.classList.toggle('burger__active');
   burgerItemns.classList.toggle('header__itemns__active');
+});
+
+burgerItemns.addEventListener('click', function(){
+  burger.classList.remove('burger__active');
+  burgerItemns.classList.remove('header__itemns__active');
+});
+
+document.addEventListener('click', function(e){
+  let click = e.composedPath().includes(burger);
+  if (!click) {
+    burger.classList.remove('burger__active');
+    burgerItemns.classList.remove('header__itemns__active');
+  }
 })
